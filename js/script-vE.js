fb7fefe1f325993f9a89b2687aedad589164ab59
Esteban Hernandez <esteban1997hdez@gmail.com>
2024-08-15 20:56:29 -0600

    Cambio de la imagen para cubrir las cartas

--- js/script-vE.js
+++ js/script-vE.js
@@ -18,7 +18,14 @@
 const MAXIMOS_INTENTOS = 20, // Intentos máximos que tiene el jugador
     COLUMNAS = 4, // Columnas del memorama
     SEGUNDOS_ESPERA_VOLTEAR_IMAGEN = 2, // Por cuántos segundos mostrar ambas imágenes
-    NOMBRE_IMAGEN_OCULTA = "./img/water/target.jpeg"; // La imagen que se muestra cuando la real está oculta
+    // NOMBRE_IMAGEN_OCULTA = "./img/water/target.jpeg"; // La imagen que se muestra cuando la real está oculta
+    NOMBRES_IMAGEN_OCULTA = [
+        "./img/air/carta-1.jpg",
+        "./img/air/carta-2.jpg",
+    ];
+
+NOMBRE_IMAGEN_OCULTA = NOMBRES_IMAGEN_OCULTA[Math.floor(Math.random() * NOMBRES_IMAGEN_OCULTA.length)];
+
 new Vue({
     el: "#app",
     data: () => ({
@@ -118,12 +125,7 @@ new Vue({
                 padding: '1rem', // Ajusta el padding para hacerlo más compacto
             }).then(this.reiniciarJuego);
         },
-        
-        
-        // Método que indica si el jugador ha ganado
-        // haGanado() {
-        //     return this.memorama.every(arreglo => arreglo.every(imagen => imagen.acertada));
-        // },
+
         haGanado() {
             return this.memorama.every(imagen => imagen.acertada);
         },
@@ -146,62 +148,7 @@ new Vue({
                 this.indicarFracaso();
             }
         },
-        // Se desencadena cuando se hace click en la imagen
-        // voltear(indiceFila, indiceImagen) {
-        //     // Si se está regresando una imagen a su estado original, detener flujo
-        //     if (this.esperandoTimeout) {
-        //         return;
-        //     }
-        //     // Si es una imagen acertada, no nos importa que la intenten voltear
-        //     if (this.memorama[indiceFila][indiceImagen].acertada) {
-        //         return;
-        //     }
-        //     // Si es la primera vez que la selecciona
-        //     if (this.ultimasCoordenadas.indiceFila === null && this.ultimasCoordenadas.indiceImagen === null) {
-        //         this.memorama[indiceFila][indiceImagen].mostrar = true;
-        //         this.ultimasCoordenadas.indiceFila = indiceFila;
-        //         this.ultimasCoordenadas.indiceImagen = indiceImagen;
-        //         return;
-        //     }
-        //     // Si es el que estaba mostrada, lo ocultamos de nuevo
-        //     let imagenSeleccionada = this.memorama[indiceFila][indiceImagen];
-        //     let ultimaImagenSeleccionada = this.memorama[this.ultimasCoordenadas.indiceFila][this.ultimasCoordenadas.indiceImagen];
-        //     if (indiceFila === this.ultimasCoordenadas.indiceFila &&
-        //         indiceImagen === this.ultimasCoordenadas.indiceImagen) {
-        //         this.memorama[indiceFila][indiceImagen].mostrar = false;
-        //         this.ultimasCoordenadas.indiceFila = null;
-        //         this.ultimasCoordenadas.indiceImagen = null;
-        //         this.aumentarIntento();
-        //         return;
-        //     }
 
-        //     // En caso de que la haya encontrado, ¡acierta!
-        //     // Se basta en ultimaImagenSeleccionada
-        //     this.memorama[indiceFila][indiceImagen].mostrar = true;
-        //     if (imagenSeleccionada.ruta === ultimaImagenSeleccionada.ruta) {
-        //         this.aciertos++;
-        //         this.memorama[indiceFila][indiceImagen].acertada = true;
-        //         this.memorama[this.ultimasCoordenadas.indiceFila][this.ultimasCoordenadas.indiceImagen].acertada = true;
-        //         this.ultimasCoordenadas.indiceFila = null;
-        //         this.ultimasCoordenadas.indiceImagen = null;
-        //         // Cada que acierta comprobamos si ha ganado
-        //         if (this.haGanado()) {
-        //             this.indicarVictoria();
-        //         }
-        //     } else {
-        //         // Si no acierta, entonces giramos ambas imágenes
-        //         this.esperandoTimeout = true;
-        //         setTimeout(() => {
-        //             this.memorama[indiceFila][indiceImagen].mostrar = false;
-        //             this.memorama[indiceFila][indiceImagen].animacion = false;
-        //             this.memorama[this.ultimasCoordenadas.indiceFila][this.ultimasCoordenadas.indiceImagen].mostrar = false;
-        //             this.ultimasCoordenadas.indiceFila = null;
-        //             this.ultimasCoordenadas.indiceImagen = null;
-        //             this.esperandoTimeout = false;
-        //         }, SEGUNDOS_ESPERA_VOLTEAR_IMAGEN * 1000);
-        //         this.aumentarIntento();
-        //     }
-        // },
         voltear(indice) {
             if (this.esperandoTimeout) {
                 return;
@@ -247,33 +194,6 @@ new Vue({
                 this.aumentarIntento();
             }
         },
-        
-        // reiniciarJuego() {
-        //     let memorama = [];
-        //     this.imagenes.forEach((imagen, indice) => {
-        //         let imagenDeMemorama = {
-        //             ruta: imagen,
-        //             mostrar: false, // No se muestra la original
-        //             acertada: false, // No es acertada al inicio
-        //         };
-        //         // Poner dos veces la misma imagen
-        //         memorama.push(imagenDeMemorama, Object.assign({}, imagenDeMemorama));
-        //     });
-
-        //     // Sacudir o mover arreglo; es decir, hacerlo aleatorio
-        //     this.mezclarArreglo(memorama);
-
-        //     // Dividirlo en subarreglos o columnas
-        //     let memoramaDividido = [];
-        //     for (let i = 0; i < memorama.length; i += COLUMNAS) {
-        //         memoramaDividido.push(memorama.slice(i, i + COLUMNAS));
-        //     }
-        //     // Reiniciar intentos
-        //     this.intentos = 0;
-        //     this.aciertos = 0;
-        //     // Asignar a instancia de Vue para que lo dibuje
-        //     this.memorama = memoramaDividido;
-        // },
 
         reiniciarJuego() {
             let memorama = [];
